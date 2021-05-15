d6cc2da9e742c1f72d173f2a7a0226ddbbe0a688
Aankhen <Aankhen@users.noreply.github.com>
2021-05-15 19:11:09 +0530

    removal: delete `src/Javascript/statesjs/`

--- src/Javascript/statesjs/meghalaya.js
+++
@@ -1,9 +0,0 @@
-LoadData();
-async function LoadData() {
-    let data = await drive('11x5AO6hY4aJ3YDq-VfTCF8_1TKBrorWWaby2JmSFpps')
-    let dataTable = document.querySelector('#data');
-    for (let dataContent = 0; dataContent < data.length; dataContent++) {
-
-        dataTable.innerHTML += "<div class='company font-mont'>" + "<p class='company-name'><span class='hide'>Company Name:</span> " + data[dataContent].companyname + "</p>" + "<p class='company-person'><span class='hide'>Person Name:</span> " + data[dataContent].personname + "</p>" + "<p class='company-number'><span class='hide'>Contact No. :</span> " + data[dataContent].contactnumber + "</p>" + "<p class='company-city'><span class='hide'>City:</span>" + data[dataContent].city + "</p>" + "</div>"
-    }
-}
