527ebcc3b3ce5a7388739ddd47c348812f4ce150
Aankhen <Aankhen@users.noreply.github.com>
2021-05-15 19:18:40 +0530

    fix: ignore JavaScript in Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
     eleventyConfig.addPassthroughCopy("./src/CSS/");
     eleventyConfig.addWatchTarget("./src/CSS/");
-    eleventyConfig.addPassthroughCopy("./src/Javascript/");
-    eleventyConfig.addWatchTarget("./src/Javascript/");
 
     return {
         dir: {
