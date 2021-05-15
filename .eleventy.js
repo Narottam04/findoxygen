b3c91e393c81107115101e8d164a16daa02d515e
Aankhen <Aankhen@users.noreply.github.com>
2021-05-15 17:52:30 +0530

    deps: add `js-yaml` to Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,16 @@
-module.exports = function (eleventyConfig) {
+const yaml = require("js-yaml");
 
-    eleventyConfig.addPassthroughCopy("./src/CSS/")
-    eleventyConfig.addWatchTarget("./src/CSS/")
-    eleventyConfig.addPassthroughCopy("./src/Javascript/")
-    eleventyConfig.addWatchTarget("./src/Javascript/")
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+    eleventyConfig.addPassthroughCopy("./src/CSS/");
+    eleventyConfig.addWatchTarget("./src/CSS/");
+    eleventyConfig.addPassthroughCopy("./src/Javascript/");
+    eleventyConfig.addWatchTarget("./src/Javascript/");
 
     return {
         dir: {
             input: "src",
             output: "public",
         },
-    }
+    };
 }
