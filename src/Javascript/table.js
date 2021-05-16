0b60269259788077025635eb6c4a8ce6a0f6573b
Narottam04 <coutkunal@gmail.com>
2021-05-16 21:50:36 +0530

    navbar issue try fix 2

--- src/Javascript/table.js
+++ src/Javascript/table.js
@@ -50,3 +50,29 @@ window.addEventListener("load", () => {
         })
         .catch(console.error);
 });
+
+
+// navbar
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-ul");
+    const navLinks = document.querySelectorAll(".nav-ul li");
+
+    // toggle nav
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+        // hamburger animation
+        burger.classList.toggle("toggle");
+        // link animation
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+                    }s`;
+            }
+        });
+    });
+};
+
+navSlide();
